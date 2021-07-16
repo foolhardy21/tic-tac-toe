902f99f32c9622c683b1ae9ca862ec27f7ee17f3
foolhardy21 <vinaybhardwaj41xii@gmail.com>
2021-07-16 13:47:04 +0530

    player-switch-working

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+let gameBoard = [
+    ['','',''],
+    ['','',''],
+    ['','','']
+]  
+let turn = true
+function getValue() {
+    return (turn) ? 'x' : 'o'
+}
 const board = document.querySelector('.grid-board')
 
 for(let i = 0; i < 3; i++) {
@@ -6,6 +15,7 @@ for(let i = 0; i < 3; i++) {
         let cellDiv = document.createElement('div')
         cellDiv.setAttribute('id','cell')
         cellDiv.setAttribute('data-position',`${i}${j}`)
+        cellDiv.innerText = gameBoard[i][j]
         cellDiv.style.background = 'lightskyblue'
         board.appendChild(cellDiv)
     }
@@ -15,7 +25,14 @@ board.addEventListener('click',(e) => {
     const targetDiv = e.target
 
     if(targetDiv.id === 'cell') {
-        console.log(targetDiv.getAttribute('data-position'))
+        let pos = targetDiv.getAttribute('data-position')
+        const row = parseInt(pos[0])
+        const col = parseInt(pos[1])
+
+        gameBoard[row][col] = getValue()
+        turn = !turn
+        targetDiv.innerText = gameBoard[row][col]
+        
     }
 })
 
